7a9852b105125d733a3eeb5b6ec90b32e2954972
Ibai Usubiaga <ibai.usubiaga@gmail.com>
2022-11-21 01:56:29 +0000

    Refactor

--- src/test/java/eus/ibai/jobs/alerts/infrastructure/metrics/MetricTestUtils.java
+++ src/test/java/eus/ibai/jobs/alerts/infrastructure/metrics/MetricTestUtils.java
@@ -2,11 +2,9 @@ package eus.ibai.jobs.alerts.infrastructure.metrics;
 
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.hamcrest.Matchers;
 import org.springframework.boot.actuate.health.Status;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.awaitility.Awaitility.await;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,13 +13,18 @@ import static org.springframework.boot.actuate.health.Status.UP;
 public class MetricTestUtils {
 
     public static void verifyComponentHealthMetricRecorded(MeterRegistry meterRegistry, String componentName, Status status) {
-        await().atMost(3, TimeUnit.SECONDS).ignoreExceptions().untilAsserted(() -> {
-            Gauge componentHealthGauge = meterRegistry.find("health.component")
-                    .tag("component", componentName)
-                    .tag("status", status.getCode())
-                    .gauge();
-            assertThat(componentHealthGauge, notNullValue());
-            assertThat(componentHealthGauge.value(), equalTo(status == UP ? 1.0d : 0.0d));
-        });
+        Gauge componentHealthGauge = meterRegistry.find("health.component")
+                .tag("component", componentName)
+                .tag("status", status.getCode())
+                .gauge();
+        assertThat(componentHealthGauge, notNullValue());
+        assertThat(componentHealthGauge.value(), equalTo(status == UP ? 1.0d : 0.0d));
+    }
+
+    public static void verifyActiveJobsRecorded(MeterRegistry meterRegistry, String siteName, int activeJobs) {
+        Gauge activeJobsGauge = meterRegistry.find("jobs.active")
+                .tag("site_name", siteName).gauge();
+        assertThat(activeJobsGauge, Matchers.notNullValue());
+        assertThat(activeJobsGauge.value(), Matchers.equalTo((double) activeJobs));
     }
 }
--- src/test/java/eus/ibai/jobs/alerts/infrastructure/metrics/MetricUtilsTest.java
+++ src/test/java/eus/ibai/jobs/alerts/infrastructure/metrics/MetricUtilsTest.java
@@ -1,6 +1,5 @@
 package eus.ibai.jobs.alerts.infrastructure.metrics;
 
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.junit.jupiter.api.Test;
@@ -11,10 +10,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static eus.ibai.jobs.alerts.TestData.JOB_SITE_1_NAME;
+import static eus.ibai.jobs.alerts.infrastructure.metrics.MetricTestUtils.verifyActiveJobsRecorded;
 import static eus.ibai.jobs.alerts.infrastructure.metrics.MetricUtils.recordActiveJobs;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 
 class MetricUtilsTest {
 
@@ -25,25 +22,22 @@ class MetricUtilsTest {
 
         recordActiveJobs(meterRegistry, JOB_SITE_1_NAME, expectedActiveJobs);
 
-        Gauge activeJobsGauge = meterRegistry.find("jobs.active").tag("site_name", JOB_SITE_1_NAME).gauge();
-        assertThat(activeJobsGauge, notNullValue());
-        assertThat(activeJobsGauge.value(), equalTo((double) expectedActiveJobs));
+        verifyActiveJobsRecorded(meterRegistry, JOB_SITE_1_NAME, expectedActiveJobs);
     }
 
     @ParameterizedTest
-    @MethodSource("provideSiteNames")
+    @MethodSource("provideSiteNamesWithWhitespaces")
     void should_replace_whitespaces_when_transforming_site_name_into_tag(String siteName, String expectedTagName) {
         MeterRegistry meterRegistry = new SimpleMeterRegistry();
+        int expectedActiveJobs = 1;
 
-        recordActiveJobs(meterRegistry, siteName, 1);
+        recordActiveJobs(meterRegistry, siteName, expectedActiveJobs);
 
-        Gauge activeJobsGauge = meterRegistry.find("jobs.active").tag("site_name", expectedTagName).gauge();
-        assertThat(activeJobsGauge, notNullValue());
+        verifyActiveJobsRecorded(meterRegistry, expectedTagName, expectedActiveJobs);
     }
 
-    private static Stream<Arguments> provideSiteNames() {
+    private static Stream<Arguments> provideSiteNamesWithWhitespaces() {
         return Stream.of(
-                Arguments.of("SiteNameWithNoSpaces", "SiteNameWithNoSpaces"),
                 Arguments.of("Site Name With One Space", "Site_Name_With_One_Space"),
                 Arguments.of("Site  Name    With      Multiple  Spaces", "Site_Name_With_Multiple_Spaces"),
                 Arguments.of("Site\nName", "Site_Name"),
